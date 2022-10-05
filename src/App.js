ae3cbdc6e103bcbc2d639af0f90ff0a77a3e8ae5
RohailAhmad-gojo <rohailgojali@gmail.com>
2022-10-05 13:57:40 +0500

    update and bugs fixed

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import Navbar from './components/navbar/Navbar';
 import Roadmap from './components/Roadmap/Roadmap';
 import Team from './components/team/Team';
 import Tree from './components/Treehouse/Tree';
-import Video from './components/videoplayer/Video';
+// import Video from './components/videoplayer/Video';
 import Wallpaper from './components/wallpaper/Wallpaper';
 import Footer from "./components/footer/Footer"
 function App() {
   return (
     <div>
       <Navbar />
-
       <Home />
-      <Video />
+      {/* <Video /> */}
       <About />
       <Featured />
       <Wallpaper />
